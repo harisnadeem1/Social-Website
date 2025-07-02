feef6df6f79e429f5d2f077b867029ddc7eea0ac
Muhammad Haris Nadeem <bscs21062@itu.edu.pk>
2025-07-02 21:28:50 +0500

    giftpop z index updated

--- frontend/src/components/chat/ChatWindow.jsx
+++ frontend/src/components/chat/ChatWindow.jsx
@@ -406,7 +406,7 @@ const ChatWindow = ({
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
-                  className="fixed inset-0 bg-black/50 z-50 lg:hidden"
+                  className="fixed inset-0 bg-black/50 z-90 lg:hidden"
                   onClick={() => setShowGiftPopup(false)}
                 />
 
